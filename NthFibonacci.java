5c7a89e5a44d910a6929d91cedc8881c41549d0b
Yujing Chen <cubthemagiclion@gmail.com>
2021-06-12 23:20:10 -0700

    Update NthFibonacci.java

--- NthFibonacci.java
+++ NthFibonacci.java
@@ -13,6 +13,23 @@ public class NthFibonacci {
 				return 1;
 			return (getNthFib(n-1) + getNthFib(n-2));
 	  }
+
+	public static int getNthFibSolution2(int n) { //O(n) time O(1) space
+		if (n == 1)
+			return 0;
+		if(n == 2)
+			return 1;
+		int previous_two = 0;
+		int previous_one = 1;
+		int current = 0;
+		for(int i = 3; i <= n; i++){
+			current = previous_one + previous_two;
+			previous_two = previous_one;
+			previous_one = current;
+
+		}
+		return current;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
